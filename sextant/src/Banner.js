61970f4fa70b478967a842799bd88eed1ecb9fbe
bessie <oghoghouzamere19@gmail.com>
2023-05-31 19:09:23 +0100

    styling of the banner

--- sextant/src/Banner.js
+++ sextant/src/Banner.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 function Banner() {
+  const bannerStyle = {
+    backgroundColor: 'blue',
+    color: 'black',
+    padding: '10px',
+    textAlign: 'center',
+    fontFamily: "Architect's Daughter, cursive",
+  };
+
+  const headingStyle = {
+    fontSize: '36px',
+    fontWeight: 'bold',
+  };
+
   return (
-    <div className="banner">
-      <h1>Sextant</h1>
+    <div className="banner" style={bannerStyle}>
+      <h1 style={headingStyle}>Sextant</h1>
     </div>
   );
 }
 
 export default Banner;
+
